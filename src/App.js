8ac15a4279489f8ab6d650e6adaf219c28dde9fb
scottjason <scottleejason@gmail.com>
2021-04-06 13:24:45 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React, { useEffect } from "react";
+import "./App.css";
+
+const ALLOWED_ORIGINS = [
+  "http://localhost:3000",
+  "https://iframe-consumer.herokuapp.com",
+];
+
+function App() {
+  useEffect(() => {
+    window.addEventListener(
+      "message",
+      function (event) {
+        const isValidOrigin = ALLOWED_ORIGINS.includes(event.origin);
+        if (!isValidOrigin || typeof event.data !== "string") {
+          return null;
+        }
+        const data = JSON.parse(event.data);
+        const isUpdateInputs = data.eventName === "updateInputs";
+        const isSubmit = data.eventName === "submit";
+        if (isUpdateInputs) {
+          console.log("updating iframe input fields");
+          document.getElementById("email").value = data.email;
+          document.getElementById("password").value = data.password;
+        } else if (isSubmit) {
+          console.log("*********");
+          console.log(
+            "submit form iframe function invoked from react app domain:",
+            event.origin
+          );
+          console.log("*********");
+          console.log(
+            "then make req from iframe domain:",
+            window.location.origin
+          );
+          console.log("*********");
+          console.log("payload received for req", data.email, data.password);
+        }
+        console.log("*********");
+      },
+      false
+    );
+  }, []);
+
+  return (
+    <div className="App">
+      <input placeholder="email" id="email"></input>
+      <input placeholder="password" id="password"></input>
+    </div>
+  );
+}
+
+export default App;
